55a64941c6f7f11993fd0e5fd57d730e4e817784
umar.techboy <umar.techboy@gmail.com>
2025-03-03 15:57:38 +0500

    Data coming (slow) and mac syncing

--- Sender/PhysMove_Sender/src/main.cpp
+++ Sender/PhysMove_Sender/src/main.cpp
@@ -5,25 +5,51 @@
 #include <Adafruit_Sensor.h>
 #include <EEPROM.h>
 
+#define NotLEDEnabled 0
+
 // Replace with the MAC address of the ESP32 receiver
+
+//204:219:167:20:61:72:
 uint8_t receiverMAC[] = {0xCC, 0xDB, 0xA7, 0x14, 0x3D, 0x48};
 
 Adafruit_MPU6050 mpu;
 
+long lastNotToggle = 0;
+bool lastLED = 0;
 void sendCallback(uint8_t *mac, uint8_t status) {
+  int d = status == 0?50:1000;
+  if (millis() - lastNotToggle > d){
+    lastNotToggle = millis();
+    lastLED = !lastLED;
+    digitalWrite(LED_BUILTIN, lastLED);
+    
     Serial.print("Send Status: ");
     Serial.println(status == 0 ? "Success" : "Failed");
+  }
 }
 
 void setup() {
-    //Serial.begin(115200, SERIAL_8N1, SerialMode::SERIAL_RX_ONLY);
+
+  // We can either have debug data displayed on the serial port or we can have the LED blinking.
+  // LED blinks every 4s = No MPU detected
+  // LED blinks every 2s = MPU connected but receiver not available
+  // LED blinks fastly = Data sending
+
+  #if NotLEDEnabled
+    Serial.begin(115200, SERIAL_8N1, SerialMode::SERIAL_RX_ONLY);
+    pinMode(LED_BUILTIN, OUTPUT);
+  #else
     Serial.begin(115200);
-    //pinMode(LED_BUILTIN, OUTPUT);
-    // delay(100);
-    // EEPROM.begin(64);
-    // for (int i = 0; i < 6; i++){
-    //   receiverMAC[i] = EEPROM.read(i);
-    // }
+  #endif
+    delay(100);
+    EEPROM.begin(64);
+    Serial.print("MAC on EEP: ");
+    for (int i = 0; i < 6; i++){
+      EEPROM.get(i, receiverMAC[i]);
+      Serial.print(receiverMAC[i]);
+      Serial.print(":");
+    }
+    Serial.println();
     WiFi.mode(WIFI_STA);
     Wire.begin(0, 2);
 
@@ -44,6 +70,12 @@ void setup() {
               break;
             }
           }
+        }
+        while(1){
+          delay(2000);
+          digitalWrite(LED_BUILTIN, 1);
+          delay(2000);
+          digitalWrite(LED_BUILTIN, 0);
         }
     }
     else
@@ -82,10 +114,34 @@ struct DataSet {
 DataSet dataPacket[DATA_SETS];
 int currentIndex = 0; // Index to track data in the packet
 
+int StrToHex(char str[])
+{
+  return (int) strtol(str, 0, 16);
+}
 long lastSample = 0;
-bool lastLED = 0;
-long lastLEDToggleAt = 0;
 void loop() {
+  // Check for MAC Update
+  if (Serial.available()){
+    // command is mac=NN:NN:NN:NN:NN:NN:
+    if(Serial.readStringUntil('=') != "mac"){
+      return;
+    }
+    
+    esp_now_del_peer(receiverMAC);
+    Serial.print("MAC Updated: ");
+    for (int i = 0; i < 6; i++){
+      char buf [16];
+      Serial.readStringUntil(':').toCharArray(buf, 6);
+      int NN = StrToHex(buf);
+      receiverMAC[i] = NN;
+      EEPROM.put(i, NN);
+      Serial.print(receiverMAC[i], 16);
+      Serial.print(":");
+    };
+    EEPROM.commit();
+    esp_now_add_peer(receiverMAC, ESP_NOW_ROLE_SLAVE, 1, NULL, 0);
+    Serial.println();
+  }
   if (millis() - lastSample >= 3){ // a packet every 3ms
     lastSample = millis();
     // Collect sensor data
